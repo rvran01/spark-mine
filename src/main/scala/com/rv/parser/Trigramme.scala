52708d90bce3fb3e32d528f7cf726d8191241752
maya <herveran@hotmail.com>
2015-02-07 08:01:19 +0100

    add a better generic decoupe method to compute trigramme

--- src/main/scala/com/rv/parser/Trigramme.scala
+++ src/main/scala/com/rv/parser/Trigramme.scala
@@ -2,14 +2,18 @@ package com.rv.parser
 
 object Trigramme {
   
-	def tri(line: String) = {
+	def perThree(line: String) = {
+    val elt = line.toLowerCase()
 		val numberTrig = line.length()-2
 		var list = List.empty[String]
 		for (i <- 0 to numberTrig-1){
-			val current  =  (""+ line.charAt(i) + line.charAt(i+1) + line.charAt(i+2))
+			val current  =  (""+ elt.charAt(i) + elt.charAt(i+1) + elt.charAt(i+2))
 				list = list ++ List(current)
 			}
 		list
 	}
-
+  
+  def decoupe(line: String, size: Int) = {
+    line.toLowerCase().sliding(size, step=1).toList
+  }
 }
--- src/test/scala/com/rv/parser/TrigrammeSpec.scala
+++ src/test/scala/com/rv/parser/TrigrammeSpec.scala
@@ -5,11 +5,30 @@ import org.scalatest.FlatSpec
 class TrigrammeSpec extends FlatSpec {
 
   it should "contain 9 elements in list" in {
-    assert(9 == (Trigramme.tri("hello world").size))    
+    assert(9 == (Trigramme.perThree("hello world").size))    
   }
   
-  it should "contain 1 element1 in list" in {
-    assert(1 == (Trigramme.tri("hel").size))    
+  it should "contain 1 elements in list" in {
+    assert(1 == (Trigramme.perThree("hel").size))    
   }
   
+  it should "contain 9 elements in list after decoupe" in {
+    val res = Trigramme.decoupe("hello world", 3)
+    assert(9 == (res.size))
+    assert(res.contains("hel"))
+    assert(res.contains("ell"))
+    assert(res.contains("llo"))
+    assert(res.contains("lo "))
+    assert(res.contains("o w"))
+    assert(res.contains(" wo"))
+    assert(res.contains("rld"))
+  }
+  
+  it should "contain the same elements : methods decoupe and perThree" in {
+    val line = "hello world"
+    val resDecoupe = Trigramme.decoupe(line, 3)
+    val resTri = Trigramme.perThree(line)
+    assert(resDecoupe.equals(resTri))
+    
+  }
 }
