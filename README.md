bc388f0abb50e1223e564a87b6181419f8248f06
maya <herveran@hotmail.com>
2015-02-10 11:46:28 +0100

    parse trigramme from candidate filename

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ The motivation: [Playing with some free files to do some map reduce stuff operat
 ###  LIST
 * BigFileCounter
 * Movie Rating
-* OccurenceCounter : use a Trigramme function to build trigramme with each line on a file, then count each occurence from 2 file filename4 and filename16
+* OccurenceCounter : use a Trigramme function to build trigramme with each line on a file, then count each occurence from filename_16
+use references to detect trigramme from candidate filename
+
--- src/main/scala/com/rv/spark/sample/SparkOccurenceCounter.scala
+++ src/main/scala/com/rv/spark/sample/SparkOccurenceCounter.scala
@@ -6,59 +6,99 @@ import com.rv.parser.Trigramme
 import scala.math.Ordering
 import org.apache.spark.rdd.RDD
 
-case class Result(sortedDescKeyByOccurences: RDD[(String, Int)], elements : RDD[(String, Int)])
+case class KeyOccurences(key : String, occurences : Int, indice : Int)
+case class Result(sortedDescKeyByOccurences: Map[String, KeyOccurences], elements : List[(String, Int)])
+case class Reference(key: String, occurencesOpt : Option[Int], indiceOpt : Option[Long])
 
 object SparkOccurenceCounter {
   
-	def getKeyWithOccurences(ctx: SparkContext, filename : String) : Result= {
+	def getKeysWithOccurences(ctx: SparkContext, filename : String) : Result= {
 		val lines = ctx.textFile(filename, 3)
 				
 		val elements = lines.flatMap { line => Trigramme.decoupe(line, size=3).map(w => (w,1)) }
 		println("number %s of elements in file %s".format(elements.toLocalIterator.size, filename))
-		
+		elements.cache()
+    
 		//group each elements by key
 		val groupsByKey = elements.groupBy(f=> f._1)
 		
 		//compute occurence
-		val keyWithOccurences = groupsByKey.toLocalIterator.toSeq.map(f=> {
+		val keyWithOccurences = groupsByKey.toLocalIterator.toList.map(f=> {
 			val key = f._1
-					val indices = f._2.toList.map(e => e._2)
-					val occurences = indices.foldLeft(0)(_+_)
-					(key, occurences)
-		}
-				)
-				
-				val keyOccurencesRDD = ctx.parallelize(seq=keyWithOccurences, numSlices=5)
-				keyOccurencesRDD.cache()
-				//sort by occurences and print with simple scala -- humm very slow
-				//keyWithOccurences.sortBy(f => f._2) (Ordering.Int).takeRight(10).foreach(f=> println("simple scala : ey(%s) - occurence(%s)".format(f._1,f._2)))
-				val sortedDescKeyByOccurences = keyOccurencesRDD.sortBy(f=> f._2, ascending=false, numPartitions=5)
-				Result(sortedDescKeyByOccurences, elements)
+			val indices = f._2.toList.map(e => e._2)
+			val occurences = indices.foldLeft(0)(_+_)
+			(key, occurences)
+		  })
+		  //sort by occurences desc
+      .sortBy(f=> f._2)(Ordering.Int.reverse)
+      //add index
+      .zipWithIndex
+      
+		val sortedDescKeyByOccurences = keyWithOccurences.map(f=> f._1._1 -> KeyOccurences(f._1._1, f._1._2, f._2)).toMap
+		 
+		Result(sortedDescKeyByOccurences, elements.toLocalIterator.toList)
 	}
   
   def compute(ctx: SparkContext, filename: String, showNumber: Int ) = {
-    val res = getKeyWithOccurences(ctx, filename)
-    res.sortedDescKeyByOccurences.take(num=10).foreach(f=> println("with rdd filename(%s): key(%s) - occurence(%s)".format(filename, f._1,f._2)))
-    println("number (%s) of elements  - number of keys(%s) in file %s".format(res.elements.toLocalIterator.size, res.sortedDescKeyByOccurences.toLocalIterator.size, filename))
+    val res = getKeysWithOccurences(ctx, filename)
+    res.sortedDescKeyByOccurences.values.take(10).foreach(f=> println("with rdd filename(%s): key(%s) - occurence(%s) - indice(%s)".format(filename, f.key, f.occurences,f.indice)))
+    println("number (%s) of elements  - number of keys(%s) in file %s".format(res.elements.size, res.sortedDescKeyByOccurences.size, filename))
+  }
+  
+  /**
+   * looking to the Reference corresponding to the toCheck key
+   */
+  def getReference(references : Map[String, KeyOccurences], toCheck : String) : Reference = {
+		//println(" *** check(%s)".format(toCheck))
+    val filteredOpt = references.get(key = toCheck)
+    filteredOpt match {
+      case Some(elt) => {
+        Reference(key = elt.key, occurencesOpt = Some(elt.occurences), indiceOpt = Some(elt.indice))
+      }
+      case None => Reference(key = toCheck, None, None)
+    }
   }
   
   def main(args: Array[String]) {
  
-  
-  val sparkConf = new SparkConf()
+    def printKeySearch(filename : String, keyToSearch : String, reference : Reference) = {
+      println("key(%s) - occurences(%s) - indice(%s) in file(%s)".format(reference.key, reference.occurencesOpt.getOrElse("NOT_FOUND"), reference.indiceOpt.getOrElse("NOT_FOUND"), filename))
+    }
+    val sparkConf = new SparkConf()
       .set("spark.executor.memory", "2g")
       .set("SPARK_LOCAL_IP", "localhost")
       .setMaster("local")
       .setAppName("Spark BigFile")
       
-  val ctx = new SparkContext(sparkConf)
-  val showNumber = 10
-  val filename4 = "/home/maya/data/fidmarques/d_export_4.txt"
-  compute(ctx, filename4, showNumber)
+    val ctx = new SparkContext(sparkConf)
+    //val showNumber = 10
+    val filename4 = "/home/maya/data/fidmarques/d_export_4.txt"
+    //compute(ctx, filename4, showNumber)
+    
+    val result4RDD = ctx.parallelize(seq=Seq(getKeysWithOccurences(ctx, filename4)), numSlices=10).cache()
+    val references = result4RDD.first().sortedDescKeyByOccurences
   
-  val filename16 = "/home/maya/data/fidmarques/d_export_16.txt"
-  compute(ctx, filename16, showNumber)
+    /*val toCheck1 = "0G "
+    printKeySearch(filename = filename4, keyToSearch = toCheck1, reference = getReference(sortedDescKeyByOccurences = keyOccurences, toCheck1))
+    */
   
-  ctx.stop()
+    //apply research on candidate file - take distinct elements
+    val candidateLinesRDD = ctx.textFile(path="/home/maya/data/fidmarques/d_export_candidate.txt", minPartitions=3)
+    val candidates = candidateLinesRDD.flatMap ( line => Trigramme.decoupe(line, size=3) ).distinct()
+    //candidates.foreach { c => printKeySearch(filename = filename4, keyToSearch = c, referenceOpt = getReference(result = result4RDD.first(), c)) }
+    def outputFormat(list : List[Reference], separator: String) : List[String]= {
+	    list.map { l =>
+        "key["+ l.key +"]" + separator + "occurences["+ l.occurencesOpt.getOrElse(-1) + "]" + separator + "indice[" + l.indiceOpt.getOrElse(-1) + "]"
+      }
+    }
+    val results = outputFormat( candidates.toLocalIterator.toList.map (c => getReference(references, toCheck = c) ), ":")
+    val resultsRDD = ctx.parallelize(seq=results.toSeq, numSlices = 1)
+    resultsRDD.saveAsTextFile("/home/maya/data/fidmarques/resultFrom4.txt" )
+ 
+  
+    //val filename16 = "/home/maya/data/fidmarques/d_export_16.txt"
+    //compute(ctx, filename16, showNumber)
+  
+    ctx.stop()
   }
 }
